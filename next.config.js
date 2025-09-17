369000b906886f57ea2a8eebb6ad6ae9e689223e
HurryWang <yuyuqaq@foxmail.com>
2025-09-18 00:56:13 +0800

    try to fix again

---
+++ next.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [{ key: "Access-Control-Allow-Origin", value: "*" }],
+      },
+    ];
+  },
+};
+
+export default nextConfig;
